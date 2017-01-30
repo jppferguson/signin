0c8971693e215f44021fffaaf7b99550db0a10eb
Jamie Ferguson <jamie.ferguson@dijam.co.uk>
2017-01-30 00:10:41 +0000

    Add rest of functionality, styles etc

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,5 @@
+import 'babel-polyfill';
+import './polyfills';
 import HideShowPassword from './modules/hideShowPassword';
 import validate from './modules/validate';
 
@@ -7,6 +9,24 @@ document.documentElement.classList.remove('no-js');
 
 // Initialise show/hide password button
 const toggleShowHidePassword = new HideShowPassword(document.querySelector('#togglePassword'));
+toggleShowHidePassword.setState(false, false);
+
+function toggleClasslist(el, className, toggle) {
+  if (toggle) {
+    el.classList.add(className, toggle);
+  } else {
+    el.classList.remove(className, toggle);
+  }
+}
+
+// Floating label pattern
+function toggleFloatingLabel(e) {
+  toggleClasslist(this.closest('.floating-label'), 'floating-label-show', e.target.value.length > 0);
+}
+Array.from(document.querySelectorAll('.floating-label .form-control')).forEach((element) => {
+  element.addEventListener('keyup', toggleFloatingLabel, false);
+  element.addEventListener('keydown', toggleFloatingLabel, false);
+});
 
 // Show trouble signing in element on click
 document.querySelectorAll('.js-show-element')[0].addEventListener('click', (e) => {
@@ -15,26 +35,32 @@ document.querySelectorAll('.js-show-element')[0].addEventListener('click', (e) =
 
 
 // Validate sign-in form
+let allowSubmit = false;
 const $signInForm = document.querySelectorAll('form')[0];
 const $errorMessage = document.getElementById('errorMessage');
 
 // Listen for submit event
 if ($signInForm) {
   $signInForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+    if (!allowSubmit) {
+      e.preventDefault();
+    }
     const $inputFields = $signInForm.querySelectorAll('input');
+    const $loadingSpinner = document.querySelector('#loadingSpinner');
+    const $submitButton = $signInForm.querySelector('[type="submit"]');
+
     // add hidden class to the error message
     $errorMessage.classList.add('hidden');
 
     // loop through each element
     const validations = Array.from($inputFields).map((element) => {
       const types = ['email', 'password'];
-      if (types.indexOf(element.dataset.validationType) > -1) {
+      if (types.indexOf(element.getAttribute('data-validation-type')) > -1) {
         return {
           $element: element,
-          minLength: element.dataset.minlength,
+          minLength: element.getAttribute('data-minlength'),
           value: element.value,
-          validationType: element.dataset.validationType,
+          validationType: element.getAttribute('data-validation-type'),
         };
       }
       return false;
@@ -50,9 +76,21 @@ if ($signInForm) {
       $el.classList.toggle('form-control-danger', !isValid);
       $el.parentNode.classList.toggle('has-danger', !isValid);
     });
+
+    // check if form is valid
     if (!isValid) {
       e.preventDefault();
       $errorMessage.focus();
+    } else if (!allowSubmit) {
+      // fake the loading:
+      // show and focus loading spinner
+      $loadingSpinner.classList.remove('hidden');
+      $loadingSpinner.focus();
+      $submitButton.setAttribute('disabled', true);
+      setTimeout(() => {
+        allowSubmit = true;
+        $signInForm.submit();
+      }, 2000);
     }
   });
 }
--- src/scripts/modules/hideShowPassword.js
+++ src/scripts/modules/hideShowPassword.js
@@ -2,30 +2,37 @@ export default class PasswordToggler {
 
   constructor(element) {
     this.element = element;
-    this.field = document.querySelectorAll(element.dataset.target)[0];
-    this.toogleClassName = element.dataset.classOff || 'shown';
-    this.ariaHide = element.dataset.ariaHide || 'Hide password';
-    this.ariaShow = element.getAttribute('aria-label');
-    this.toggle();
-  }
-
-  toggle() {
-    const self = this;
-    if (self.element) {
-      self.element.addEventListener('click', (e) => {
-        // get the current state
-        const shown = self.element.dataset.shown === 'true';
-        // reverse it
-        self.element.dataset.shown = !shown;
-        // swap the state
-        self.field.setAttribute('type', shown ? 'text' : 'password');
-        // set the aria label
-        self.element.setAttribute('aria-label', shown ? this.ariaShow : this.ariaHide);
-        // toggle class
-        self.element.classList.toggle(self.toogleClassName, shown);
+    this.shown = this.element.getAttribute('data-shown') === 'true';
+    this.field = document.querySelectorAll(element.getAttribute('data-target'))[0];
+    this.toogleClassName = element.getAttribute('data-class-off') || 'shown';
+    this.ariaLabelledby = document.querySelector(this.element.getAttribute('aria-labelledby'));
+    this.ariaHide = this.ariaLabelledby.ariaHide || 'Hide password';
+    this.ariaShow = this.ariaLabelledby.innerText;
+    if (this.element) {
+      this.element.addEventListener('click', (e) => {
+        // set the state to the reverse
+        this.setState(!this.shown);
         // prevent default button events
         e.preventDefault();
       }, false);
+    }
+  }
+
+  setState(shown, setFocus = true) {
+    const self = this;
+    if (self.element) {
+      if (setFocus) {
+        // focus back on the field
+        self.field.focus();
+      }
+      // swap the state
+      self.field.setAttribute('type', shown ? 'text' : 'password');
+      // set the aria label (delayed slightly)
+      self.ariaLabelledby.innerText = !shown ? this.ariaShow : this.ariaHide;
+      // toggle class
+      self.element.classList.toggle(self.toogleClassName, shown);
+      // update the state
+      self.shown = shown;
     }
   }
 
